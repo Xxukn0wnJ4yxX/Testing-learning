7f71598472a865074701a25aa73036c651f61052
Xx_unkn0wnJ4y_xX <jamesmraz6973@gmail.com>
2025-10-25 03:17:38 +0000

    FINALLY

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-const myCanvas = document.getElementById("myCanvas");
-const ctx = myCanvas.getContext("2d");
-
-ctx.fillRect(20, 20, 150, 100);
+// HOW THE FUCK DO I MAKE A FUCKING RECTANGLE???
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 100, 50);
