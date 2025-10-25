c1db0c75916ae6dde40a071179c7fe04e2e08220
Xx_unkn0wnJ4y_xX <jamesmraz6973@gmail.com>
2025-10-25 16:02:56 +0000

    b

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-// HOW THE FUCK DO I MAKE A FUCKING RECTANGLE???
 const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 100, 50);
+const rect = canvas.getContext("2d");
+rect.fillStyle = "red";
+rect.fillRect(50, 50, 100, 50);
+//the pain now is making it move
